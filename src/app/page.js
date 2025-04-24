4210ac339aabfbc7185924198e12acaebba2bd21
faty <145115890+fatoumatad223@users.noreply.github.com>
2025-04-24 19:50:30 +0200

    add: home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,8 @@
 import Head from "next/head"
-import styles from "../styles/Banner.module.css"
+import Banner from "@/components/Banner"
+import Searchbar from "@/components/SearchBar"
+import Animals from "@/components/Animals"
+import Fonctionnement from "@/components/Fonctionnement"
 
 import { Amatic_SC } from 'next/font/google'
 const amatic = Amatic_SC({
@@ -14,7 +17,6 @@ const roboto = Roboto({
 })
 
 
-
 export default function Home() {
     return (
         <>
@@ -26,19 +28,10 @@ export default function Home() {
                 />
             </Head>
 
-            <div className={styles.home}>
-                <img className={styles.banner} src="./assets/background.jpg" alt="Image d'un chien" />
-                <h1 className={styles.title}>DONNONS-LEUR AUTANT QU'ILS NOUS APPORTENT</h1>
-                <p className={styles.paragraphe}>
-                    Chaque jour, des milliers d'animaux attendent une famille aimante.
-                    Trouver votre compagnon idéal parmi nos animaux disponibles à l'adoption.
-                </p>
-            </div>
-
-            <div className={styles.search}>
-
-            </div>
-
+            <Banner />
+            <Searchbar />
+            <Animals />
+            <Fonctionnement />
         </>
     )
 }
