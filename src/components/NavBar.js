67402788b6f435b7f250b47a98c32ddbcbc37df0
faty <145115890+fatoumatad223@users.noreply.github.com>
2026-01-16 22:26:07 +0100

    project front done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,11 +8,13 @@ import styles from "../styles/Navbar.module.css"
 export default function Navbar() {
     const [open, setOpen] = useState(false);
 
+    const handleLinkClick = () => {
+        setOpen(false); // ferme le menu burger
+    };
+
     return (
         <nav className={styles.navbar}>
-            <h2 className={styles.h2}>
-                <PawPrint /> Ada<span className={styles.span}>opte</span>
-            </h2>
+            <h2 className={styles.h2}><PawPrint /> Ada<span className={styles.span}>opte</span></h2>
 
             <div className={styles.burger} onClick={() => setOpen(!open)}>
                 <span></span>
@@ -21,17 +23,12 @@ export default function Navbar() {
             </div>
 
             <ul className={`${styles.list} ${open ? styles.open : ""}`}>
-                <li><Link className={styles.link} href="/"><House /> Accueil</Link></li>
-                <li><Link className={styles.link} href="/adopt"><Bone /> J'adopte</Link></li>
-                <li><Link className={styles.link} href="https://www.defensedelanimal.fr/wp-content/uploads/2022/03/Guide-de-la-belle-adoption-WEB.pdf" target="_blank"><Fish /> Guide de l'adoption</Link></li>
-                <li><Link className={styles.link} href="/volunteer"><Squirrel /> Devenir bénévole</Link></li>
-
-                {/* donation DANS le burger */}
-                <li className={styles.donationMobile}>Faire un don 🫶🏽</li>
+                <li><Link className={styles.link} href="/" onClick={handleLinkClick}><House /> Accueil</Link></li>
+                <li><Link className={styles.link} href="/adopt" onClick={handleLinkClick}><Bone /> J'adopte</Link></li>
+                <li><Link className={styles.link} href="https://www.defensedelanimal.fr/wp-content/uploads/2022/03/Guide-de-la-belle-adoption-WEB.pdf" target="_blank" onClick={handleLinkClick}><Fish /> Guide de l'adoption</Link></li>
+                <li><Link className={styles.link} href="/volunteer" onClick={handleLinkClick}><Squirrel />Devenir bénévole</Link></li>
+                <li><p className={styles.donation} onClick={handleLinkClick}>Faire un don 🫶🏽</p></li>
             </ul>
-
-            {/* donation DESKTOP */}
-            <p className={styles.donation}>Faire un don 🫶🏽</p>
         </nav>
     )
 }
